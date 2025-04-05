68db2c406b1e492e6c02f1bb4783bc0f93473c3c
yeasinmiah1272001 <137316520+yeasinmiah1272001@users.noreply.github.com>
2025-04-05 17:39:56 +0600

    get all todo

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,10 +1,44 @@
+import todomodel from "../models/todoModels.js";
+
 const addTodo = async (req, res) => {
   try {
-    res.json({ success: true, message: "todo added succes" });
+    const { mark, subjectName, rollNumber, name } = req.body;
+    if (!name) {
+      return res.json({ success: false, message: "name is required" });
+    }
+    if (!rollNumber) {
+      return res.json({ success: false, message: "rollNumber is required" });
+    }
+    if (!subjectName) {
+      return res.json({ success: false, message: "subject  is required" });
+    }
+    if (!mark) {
+      return res.json({ success: false, message: "mark is required" });
+    }
+
+    const newTodo = new todomodel({ name, subjectName, rollNumber, mark });
+    await newTodo.save();
+    return res.json({ success: true, message: "todo added succes" });
   } catch (error) {
     console.log("add todo error", error);
     res.json({ success: false, message: "add todo error" });
   }
 };
 
-export { addTodo };
+const getTodo = async (req, res) => {
+  try {
+    const total = await todomodel.countDocuments({});
+    const todo = await todomodel.find({});
+    return res.json({
+      success: true,
+      message: "get all todo succes",
+      total,
+      todo,
+    });
+  } catch (error) {
+    console.log("get todo error", error);
+    res.json({ success: false, message: "get todo error" });
+  }
+};
+
+export { addTodo, getTodo };
--- routes/todoRouter.js
+++ routes/todoRouter.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { addTodo } from "../controllers/todoController.js";
+import { addTodo, getTodo } from "../controllers/todoController.js";
 
 const todoRouter = express.Router();
 
 todoRouter.post("/addTodo", addTodo);
 // todoRouter.delete("/deleteTodo/:id");
 // todoRouter.put("/updateTodo/:id");
-// todoRouter.get("/allTodo");
+todoRouter.get("/allTodo", getTodo);
 
 export default todoRouter;
