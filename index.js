7a1e79cdf09fd8f6d417e4ef47e542dec268a4bb
yeasinmiah1272001 <137316520+yeasinmiah1272001@users.noreply.github.com>
2025-04-05 17:20:45 +0600

    database connect success

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import todoRouter from "./routes/todoRouter.js";
+import dbConnect from "./config/databaseConnect.js";
 
 const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
-
+dbConnect();
 app.get("/", (req, res) => {
   res.send("Todo Server is Running!");
 });
